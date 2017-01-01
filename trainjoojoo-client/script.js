702612b5e4e2caf6d1757133a1f79bffb7224d2c
Carl Bolduc <cbol@me.com>
2017-01-01 14:12:17 -0500

    Display body parts on load

--- trainjoojoo-client/script.js
+++ trainjoojoo-client/script.js
@@ -1,4 +1,4 @@
-var exercises = document.getElementById("exercises");
+var bodyParts = document.getElementById("body-parts");
 var getExercisesButton = document.getElementById("get-exercises");
 
 var request = new XMLHttpRequest();
@@ -6,18 +6,17 @@ var request = new XMLHttpRequest();
 request.onreadystatechange = function() {
     if (request.readyState === 4) {
         if (request.status === 200) {
-            var serverExercises = JSON.parse(request.responseText);
-            for (var i =0; i<serverExercises.length; i++) {
-                exercises.innerHTML += "<li>" + serverExercises[i].name + "</li>";
+            var serverBodyParts = JSON.parse(request.responseText);
+            for (var i =0; i < serverBodyParts.length; i++) {
+              var newDiv = document.createElement('div');
+              newDiv.setAttribute('class', 'body-part');
+              newDiv.innerHTML = serverBodyParts[i].name;
+              bodyParts.appendChild(newDiv);
             }
-            console.log(serverExercises[0].name);
         }
     }
 }
 
-request.open("GET", "http://localhost:3000/exercises");
+request.open("GET", "http://localhost:3000/body_parts");
 request.setRequestHeader("Accept","application/json");
-
-getExercisesButton.addEventListener("click", function() {
-    request.send();
-});
+request.send();
