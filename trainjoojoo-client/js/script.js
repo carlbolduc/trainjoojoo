8658a680609631b348a8a0146aafab70fe8038e2
Carl Bolduc <carl.bolduc@frimastudio.com>
2017-01-27 13:05:40 -0500

    Remove jQuery and use plain JavaScript instead

--- trainjoojoo-client/js/script.js
+++ trainjoojoo-client/js/script.js
@@ -1,24 +1,31 @@
 var API_URL = "http://localhost:3000";
+var serverBodyParts = [];
 
-$(function() {
+var xhrBodyPartsTemplate = new XMLHttpRequest();
+xhrBodyPartsTemplate.open('GET', 'templates/bodyParts.mst');
+xhrBodyPartsTemplate.onload = function() {
+    if (xhrBodyPartsTemplate.status === 200) {
+	//document.getElementById('foobar').parentNode.removeChild(document.getElementById('foobar'));
+	//may be useful: http://stackoverflow.com/questions/30960225/mustache-js-external-template-without-jquery
+	var rendered = Mustache.render(JSON.parse(xhrBodyPartsTemplate.responseText), { bodyParts: serverBodyParts });
+	document.getElementById('body-parts').appendChild(rendered);
+    }
+    else {
+        alert('Request failed.  Returned status of ' + xhrBodyPartsTemplate.status);
+    }
+};
 
-    getBodyParts();
+var xhrServerBodyParts = new XMLHttpRequest();
+xhrServerBodyParts.open('GET', apiUrl + '/body_parts');
+xhrServerBodyParts.onload = function() {
+    if (xhrServerBodyParts.status === 200) {
+	serverBodyParts = JSON.parse(xhrServerBodyParts.responseText);
+	xhrBodyPartsTemplate.send();
+    }
+    else {
+        alert('Request failed.  Returned status of ' + xhrServerBodyParts.status);
+    }
+};
 
-});
 
-
-function getBodyParts() {
-    $.ajax({
-        url: apiUrl + "/body_parts",
-        success: function(data) {
-            loadBodyParts(data);
-        }
-    });
-}
-
-function loadBodyParts(serverBodyParts) {
-  $.get('templates/bodyParts.mst', function(template) {
-    var rendered = Mustache.render(template, { bodyParts: serverBodyParts });
-    $('#body-parts').html(rendered);
-  });
-}
+xhrServerBodyParts.send();
